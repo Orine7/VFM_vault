c934908bfff6b56ca6a1543ec2be2605ad4bc4a5
Orine7 <eduardofilho_08@hotmail.com>
2022-05-14 11:42:03 -0300

    vault backup: 2022-05-14 11:42:03

--- _Storage/Scripts/generate_Portrait.js
+++ _Storage/Scripts/generate_Portrait.js
@@ -1,5 +1,31 @@
 function generatePortrait(name, img) {
-  return name + ' ' + img
+  console.log('Generating portrait for ' + name)
+  console.log('Image: ' + img)
+  //   return `<table width=100% style="border-collapse: collapse; border: none;" >
+  //         <tbody>
+  //         <tr style="border: none;">
+  //             <td width=50% style="border: none;">Title: ${name} </td>
+  //             <td rowspan="6" width=50% style="border: none;">
+  //                 <img src="${img}" style="max-height: 400px; max-width: 400px;" >
+  //             </td>
+  //         </tr>
+  //         <tr>
+  //             <td>Event Period: </td>
+  //         </tr>
+  //         <tr>
+  //             <td>Event Type: </td>
+  //         </tr>
+  //         <tr>
+  //             <td>Region: </td>
+  //         </tr>
+  //         <tr>
+  //             <td>Causes</td>
+  //         </tr>
+  //         <tr>
+  //             <td>Effects</td>
+  //         </tr>
+  //         </tbody>
+  //     </table>`
 }
 
 module.exports = generatePortrait
